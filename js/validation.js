08b563ddad2bd856debaba93415a8019c0357394
ashilkn <ashilkn99@gmail.com>
2021-11-29 19:01:52 +0530

    29-nov-2021

--- js/validation.js
+++ js/validation.js
@@ -5,19 +5,55 @@ function validation() {
     var message = document.forms["RegForm"]["message"];
     var name = document.forms["RegForm"]["name"];
     var email = document.forms["RegForm"]["email"];
+    var contactNumber = document.forms["RegForm"]["contactNumber"];
     var spaceCounter = 0;
     var flag = 0;
+    var flag1 = 0;
+    var emailErr = document.getElementById('email-err');
+    var nameErr = document.getElementById('name-err');
+    var messageErr = document.getElementById('message-err');
+
+    emailErr.className = "";
+    nameErr.className = "";
+    messageErr.className = "";
 
     message.style.border = "1px #66FF99 solid";
     name.style.border = "1px #66FF99 solid";
     email.style.border = "1px #66FF99 solid";
+    contactNumber.style.border = "1px #66FF99 solid";
 
-    if(message.value == "" && name.value == "" && email.value == "") {
+    if(message.value == "" && name.value == "" && email.value == "" && contactNumber.value == "") {
+        messageNotEntered();
+        nameNotEntered();
+        emailNotEntered();
+        numberNotEntered();
+        return false;
+    }
+
+    if(message.value == "" && name.value == "" && email.value == ""){
         messageNotEntered();
         nameNotEntered();
         emailNotEntered();
         return false;
+    }
 
+    if(message.value == "" && name.value == "" && contactNumber.value == "") {
+        messageNotEntered();
+        nameNotEntered();
+        numberNotEntered();
+        return false;
+    }
+    if(message.value == "" && email.value == "" && contactNumber.value == "") {
+        messageNotEntered();
+        emailNotEntered();
+        numberNotEntered();
+        return false;
+    }
+    if(name.value == "" && email.value == "" && contactNumber.value == "") {
+        nameNotEntered();
+        emailNotEntered();
+        numberNotEntered();
+        return false;
     }
 
     if(message.value == "" && name.value == "") {
@@ -38,6 +74,24 @@ function validation() {
         return false;
     }
 
+    if(contactNumber.value == "" && message.value == "") {
+        numberNotEntered();
+        messageNotEntered();
+        return false;
+    }
+
+    if(contactNumber.value == "" && name.value == "") {
+        numberNotEntered();
+        nameNotEntered();
+        return false;
+    }
+
+    if(contactNumber.value == "" && email.value == "") {
+        emailNotEntered();
+        messageNotEntered();
+        return false;
+    }
+
     if (message.value == "") {
         messageNotEntered();
         return false;
@@ -53,13 +107,39 @@ function validation() {
         return false;
     }
 
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(RegForm.email.value))
-    {}
-    else {
-        var forBorder = document.getElementById("email");
-        forBorder.style.borderColor = "red";
-        forBorder.placeholder = "INVALID EMAIL";
+    if(contactNumber.value == ""){
+        numberNotEntered();
         return false;
+    }
+
+    if(1){
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(RegForm.email.value)))
+        {
+            var forBorder = document.getElementById("email");
+            forBorder.style.borderColor = "red";
+            forBorder.placeholder = "INVALID EMAIL";
+            emailErr.className = "appear";
+            flag1 = 1;
+        }
+
+        if(/[0-9]/.test(name.value)) {
+            nameErr.className = "appear";
+            var forBorder = document.getElementById("name");
+            forBorder.style.borderColor = "red";
+            flag1 = 1;
+        }
+
+        if(message.value[0] == " ")
+        {
+            var forBorder = document.getElementById("message");
+            forBorder.style.borderColor = "red";
+            messageErr.className = "appear";
+            flag1 = 1;
+        }
+        
+        if(flag1 == 1){
+            return false;
+        }
     }
 
     for(var i = 0 ; i < String(message.value).length; i++) {
@@ -78,18 +158,14 @@ function validation() {
         message.style.border = "1px #66FF99 solid";
     }
 
-    for(var i = 0; i < String(name.value).length; i++) {
-        if(name.value[i] == 0 || name.value[i] == 1 || name.value[i] == 2 || name.value[i] == 3 || name.value[i] == 4 || name.value[i] == 5 || name.value[i] == 6 || name.value[i] == 7 || name.value[i] == 8 || name.value[i] == 9 ) {
-            flag = 1;
-            break;
-        }
-    }
+    message.style.border = "1px #66FF99 solid";
+    name.style.border = "1px #66FF99 solid";
+    email.style.border = "1px #66FF99 solid";
+    contactNumber.style.border = "1px #66FF99 solid";
 
-    if( flag == 1 ) {
-        var forBorder = document.getElementById("name");
-        forBorder.style.borderColor = "red";
-        return false;
-    }
+    setTimeout(function(){
+        alert("From submitted!");
+    }, 500)
     return true;
 }
 
@@ -109,4 +185,10 @@ function emailNotEntered() {
     var forBorder = document.getElementById("email");
     forBorder.style.borderColor = "red";
     forBorder.placeholder = "EMAIL NOT ENTERED!";
+}
+
+function numberNotEntered() {
+    var forBorder = document.getElementById("contactNumber");
+    forBorder.style.borderColor = "red";
+    forBorder.placeholder = "CONTACT NOT ENTERED!";
 }
